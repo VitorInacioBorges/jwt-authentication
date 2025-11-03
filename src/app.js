07a24d72fe53b3fe52026bddac629061abc6a954
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-11-03 11:19:15 -0300

    feat: added /src/app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 // configures express and exports all routes and middlewares.
+
+import express from "express";
+import user_routes from "./routes/user_routes.js";
+import error_middleware from "./middlewares/error_middleware.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", user_routes);
+app.use(error_middleware);
+
+export default app;
