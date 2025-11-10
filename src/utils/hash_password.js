e7093fe75ac8edbd536773368629a4c34c9f7d3f
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-11-10 09:41:12 -0300

    refactor: refactores sal rounds encryption

--- src/utils/hash_password.js
+++ src/utils/hash_password.js
@@ -10,7 +10,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export function hashPassword(password) {
-  return bcrypt.hashSync(password, process.env.BCRYPT_SALT_ROUNDS); // generates a secure hash with a salt of 10
+  return bcrypt.hashSync(password, process.env.BCRYPT_SALT_ROUNDS || 10); // generates a secure hash with salt rounds specified or 10
 }
 
 export function compareHashedPassword(password, hashedPassword) {
